58470a613ce0c48a387c3bc8190c138ce57cc012
Dmitry Kilin <coding@kilin.biz>
2022-11-12 13:05:53 +0300

    fix: check SelectList tabs selection

--- src/components/header/components/form-search/components/select-list/select-list.tsx
+++ src/components/header/components/form-search/components/select-list/select-list.tsx
@@ -1,5 +1,5 @@
 import {Camera} from '../../../../../../types/camera';
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {RootRouterPath} from '../../../../../../routers/root-route-path';
 
 type Props = {
@@ -8,13 +8,24 @@ type Props = {
 
 function SelectList(props: Props): JSX.Element {
   const {items} = props;
-  const navigate = useNavigate();
   const getVisibilityAttribute = () => items.length > 0 ? 'visible' : 'hidden';
   const getOpacityAttribute = () => items.length > 0 ? '1' : '0';
-  const generateListItemClickHandler = (id: number) => () => navigate(`${RootRouterPath.Product}/${id}`);
+  const generateProductLinkPath = (id: number) => `${RootRouterPath.Product}/${id}`;
   return (
-    <ul className="form-search__select-list" style={{visibility: getVisibilityAttribute(), opacity: getOpacityAttribute()}}>
-      {items.map((item) => <li className="form-search__select-item" onClick={generateListItemClickHandler(item.id)} tabIndex={0} key={item.id}>{item.name}</li>)}
+    <ul className="form-search__select-list"
+      style={{visibility: getVisibilityAttribute(), opacity: getOpacityAttribute()}}
+    >
+      {
+        items.map((item) =>
+          (
+            <li className="form-search__select-item" tabIndex={-1} key={item.id}>
+              <Link to={generateProductLinkPath(item.id)}>
+                {item.name}
+              </Link>
+            </li>
+          )
+        )
+      }
     </ul>
   );
 }
