e628159af79680e35474a62173e2d7bcffc1c5c0
Dmitry Kilin <coding@kilin.biz>
2022-12-01 18:38:03 +0300

    feat: Basket test runs successfully

--- src/pages/basket/busket.test.tsx
+++ src/pages/basket/busket.test.tsx
@@ -1,13 +1,27 @@
 import {render, screen} from '@testing-library/react';
 import Basket from './basket';
+import {createMemoryRouter, RouterProvider} from 'react-router-dom';
 
 describe('Component: Basket', () => {
 
   it('should render correctly', () => {
-    render(
-      <Basket/>
+    const testRouter = createMemoryRouter(
+      [
+        {
+          path: '/',
+          element: <Basket/>,
+        },
+      ],
+      {
+        initialEntries: ['/'],
+        initialIndex: 0
+      }
     );
 
-    expect(screen.getByText(/BASKET/i)).toBeInTheDocument();
+    render(
+      <RouterProvider router={testRouter}/>
+    );
+
+    expect(screen.getByText(/Корзина/i)).toBeInTheDocument();
   });
 });
